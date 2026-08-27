e8c151bbc75cabdbec7ce902caa6e412c2888169
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-08-27 18:07:31 +0430

    update

--- game.js
+++ game.js
@@ -249,6 +249,11 @@ function checkCollisions(){
 
                 bullet.x < enemy.x + enemy.width &&
 
+                bullet.x + bullet.width > enemy.x &&
+
+                bullet.y < enemy.y + enemy.height &&
+
+                bullet.y + bullet.height > enemy.y
 setInterval(()=>{
 
     createEnemy();
