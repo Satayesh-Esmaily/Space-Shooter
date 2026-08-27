24ba67a2212b0b7eb70a4219c13fb95709783871
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-08-27 19:01:58 +0430

    add

--- game.js
+++ game.js
@@ -20,6 +20,25 @@ let score = 0;
 let health = 100;
 let gameOver = false;
 
+
+const stars = [];
+
+for(let i = 0; i < 100; i++){
+
+    stars.push({
+
+        x: Math.random() * canvas.width,
+
+        y: Math.random() * canvas.height,
+
+        size: Math.random() * 3,
+
+        speed: Math.random() * 2 + 1
+
+    });
+
+}
+
 // Draw player
 function drawPlayer() {
 
