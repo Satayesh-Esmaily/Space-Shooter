6713b618f8a2c1fadfb71b480f1b45da91a4417c
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-08-27 17:42:10 +0430

    add

---
+++ game.js
@@ -0,0 +1,81 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+
+// Player object
+const player = {
+    x: 370,
+    y: 520,
+    width: 60,
+    height: 40,
+    speed: 5
+};
+
+
+// Draw player
+function drawPlayer() {
+
+    ctx.fillStyle = "cyan";
+
+    ctx.fillRect(
+        player.x,
+        player.y,
+        player.width,
+        player.height
+    );
+}
+
+
+// Move player
+const keys = {};
+
+window.addEventListener("keydown", (event) => {
+    keys[event.key] = true;
+});
+
+
+window.addEventListener("keyup", (event) => {
+    keys[event.key] = false;
+});
+
+
+function updatePlayer() {
+
+    if (keys["ArrowLeft"] && player.x > 0) {
+        player.x -= player.speed;
+    }
+
+
+    if (
+        keys["ArrowRight"] &&
+        player.x + player.width < canvas.width
+    ) {
+        player.x += player.speed;
+    }
+}
+
+
+
+// Game loop
+function gameLoop() {
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    updatePlayer();
+    drawPlayer();
+
+
+    requestAnimationFrame(gameLoop);
+}
+
+
+gameLoop();
